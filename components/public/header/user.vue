dd3a0a7520cbc51d53ae9e457ffae69ce8a68edb
shenzq <shenzq@citycloud.com.cn>
2018-12-30 22:59:55 +0800

    feat(geo,user): 城市及用户

---
+++ components/public/header/user.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="m-user">
+    <template v-if="user">{{ user }}
+      欢迎您,<nuxt-link to="./exit">退出</nuxt-link>
+    </template>
+    <template v-else>
+      <nuxt-link 
+        to="./login" 
+        class="login">
+        立即登录
+      </nuxt-link>
+      <nuxt-link 
+        class="register" 
+        to="./register">注册
+      </nuxt-link>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			user: '沈小强'
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
